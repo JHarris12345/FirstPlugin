72b323e924f9790afbb57218b31debcfc1e2cd8b
JHarris12345 <j0shuah@hotmail.co.uk>
2021-03-10 04:19:07 +0000

    first commit

---
+++ src/main/java/me/jharris/doubledrops/Main.java
@@ -0,0 +1,19 @@
+package me.jharris.doubledrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hello World!");
+
+        getServer().getPluginManager().registerEvents(new BreakBlock(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
