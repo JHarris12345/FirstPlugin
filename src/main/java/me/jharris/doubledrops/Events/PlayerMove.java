f99e49e7251b5f82ba5012a95cf549796372534c
JHarris12345 <j0shuah@hotmail.co.uk>
2021-03-11 20:46:43 +0000

    Update PlayerMove.java

--- src/main/java/me/jharris/doubledrops/Events/PlayerMove.java
+++ src/main/java/me/jharris/doubledrops/Events/PlayerMove.java
@@ -12,6 +12,10 @@ public class PlayerMove implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         Player p = event.getPlayer();
 
+        if(event.getFrom().getBlockX() == event.getTo().getBlockX() && event.getFrom().getBlockZ() == event.getTo().getBlockZ() && event.getFrom().getBlockY() == event.getTo().getBlockY()) {
+            return;
+        }
+
         if (!p.hasPermission("doubledrops.move")) {
             event.setCancelled(true);
             p.sendMessage(ChatColor.RED + "You do not have permission to move in this world!");
